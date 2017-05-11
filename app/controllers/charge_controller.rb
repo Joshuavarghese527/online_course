39c1b8e9e41db39580a0d3abb4e491a88dde41b3
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-11 13:05:40 +0000

    Updating Task Controller

--- app/controllers/charge_controller.rb
+++ app/controllers/charge_controller.rb
@@ -1,9 +1,9 @@
 class ChargeController < ApplicationController
 before_action :authenticate_user! #user needs to be logged in
 
-  def free
+def free
     project = Project.find(params[:project_id])
-    current_user.subscription.create(project: project)
+    current_user.subscriptions.create(project: project)
 
     redirect_to project
   end
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,10 +1,25 @@
 class TaskController < ApplicationController
+  before_action :authenticate_users! 
+
    def show
     project = Project.find(params[:project_id])
     @tasks = project.tasks.order(:tag)
+
+      joined = false #used @ only when you going to use it for the view. Can still use it but not necessary 
+
+    if !current_user.nil? && !current_user.projects.nil?
+      @joined = current_user.projects.include?(project) #if current user joined the project or not
+    end
+
+    if joined
+
+
     @task = @tasks.find(params[:id])
 
     @next_task = @task.next
     @prev_task = @task.prev
+  else
+    flash[:notice] = "No permission to this"
+    redirect_to project
   end
 end
