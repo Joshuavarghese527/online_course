140bd4c6d0e3156503823b10f7be7a4b1cae49fa
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-11 13:23:14 +0000

    Creating my projects page

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,25 +1,25 @@
 class TaskController < ApplicationController
-  before_action :authenticate_users! 
+  before_action :authenticate_user!
 
-   def show
+  def show
     project = Project.find(params[:project_id])
     @tasks = project.tasks.order(:tag)
 
-      joined = false #used @ only when you going to use it for the view. Can still use it but not necessary 
+    joined = false
 
     if !current_user.nil? && !current_user.projects.nil?
-      @joined = current_user.projects.include?(project) #if current user joined the project or not
+      joined = current_user.projects.include?(project)
     end
 
     if joined
 
+      @task = @tasks.find(params[:id])
 
-    @task = @tasks.find(params[:id])
-
-    @next_task = @task.next
-    @prev_task = @task.prev
-  else
-    flash[:notice] = "No permission to this"
-    redirect_to project
+      @next_task = @task.next
+      @prev_task = @task.prev
+    else
+      flash[:notice] = "No permission to this!"
+      redirect_to project
+    end
   end
 end
