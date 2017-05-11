549260c267f97baa3c9e404a89c9741e22aa9bc1
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-11 16:56:22 +0000

    take out email for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,7 @@ class User < ApplicationRecord
 
   after_create :send_notification
 
-  def send_notification
-    MyMailer.new_user(self).deliver
-  end
+ 
 
   def self.find_for_google_oauth2(access_token, signed_in_resourse=nil)
     data = access_token.info
