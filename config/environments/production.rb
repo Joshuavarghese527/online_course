bfacf5d5be9b6449f974f143ae4c02e4a90bf248
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-12 19:13:02 +0000

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'https://worship-course.herokuapp.com/'}
 
-config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    enable_starttls_auto: true,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: 'plain'
-}
-
+ # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://worship-course.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   config.paperclip_defaults = {
   :storage => :s3,
