85b8e2fcb5b75ddf4553f8aee982e8ef77e4a3a7
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-12 17:47:14 +0000

    AWS for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,17 @@ config.action_mailer.smtp_settings = {
     authentication: 'plain'
 }
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
