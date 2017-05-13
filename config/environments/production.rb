769df8f468b7a60d102cc48a896ad049ffc007cc
Joshua Varghese <joshuavarghese527@gmail.com>
2017-05-13 03:56:13 +0000

    Added sendgrid production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://worship-course.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
